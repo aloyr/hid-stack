b37b19f0a97659ee18471d2cd072395becd72f4e
Peter Carrero <peter.carrero@gmail.com>
2017-08-29 09:57:17 -0500

    Add output to sudo commands

--- prep-macos.bash
+++ prep-macos.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 HOSTMACHINE_IP="172.16.123.45"
+echo "creating loopback alias if needed"
 sudo ifconfig lo0 alias $HOSTMACHINE_IP
-echo "nameserver $HOSTMACHINE_IP" | sudo tee /etc/resolver/dev
+echo "setting up local dns resolution if needed"
+echo "nameserver $HOSTMACHINE_IP" | sudo tee /etc/resolver/dev > /dev/null
 
