fb2e3fee903fe6d5971798373bc6b10450604533
Peter Carrero <peter.carrero@gmail.com>
2017-11-16 14:26:50 -0600

    Adjust startup file

--- prep-macos.bash
+++ prep-macos.bash
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-HOSTMACHINE_IP="172.16.123.45"
-echo "creating loopback alias if needed"
-sudo ifconfig lo0 alias $HOSTMACHINE_IP
-echo "setting up local dns resolution if needed"
-echo "nameserver $HOSTMACHINE_IP" | sudo tee /etc/resolver/dev > /dev/null
+DIR=$(dirname $0)
+
 echo "checking config files"
 function checkfiles() {
 if [ ! -f $1 ]; then
@@ -12,9 +9,16 @@ if [ ! -f $1 ]; then
   echo "file created with default settings: $1"
 fi
 }
-checkfiles "config/default.conf" "config-slash-default.conf.example"
-checkfiles "config/settings.php" "config-slash-settings-d7.php.example"
-checkfiles ".env" "dot-env.example"
+checkfiles "$DIR/config/default.conf" "$DIR/config-slash-default.conf.example"
+checkfiles "$DIR/config/settings.php" "$DIR/config-slash-settings-d7.php.example"
+checkfiles "$DIR/.env" "$DIR/dot-env.example"
+
+HOSTMACHINE_IP="172.16.123.45"
+. $DIR/.env
+echo "creating loopback alias if needed"
+sudo ifconfig lo0 alias $HOSTMACHINE_IP
+echo "setting up local dns resolution if needed"
+echo "nameserver $HOSTMACHINE_IP" | sudo tee /etc/resolver/dev > /dev/null
 
 echo ""
 echo "if any files were created with the default settings, you will need to customize them before this stack is usable"
