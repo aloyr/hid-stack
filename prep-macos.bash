2e09b3988627bfc39cf8b493bbc93051046302f1
Peter Carrero <peter.carrero@gmail.com>
2018-05-31 11:31:21 -0500

    Remove commands from prep-macos

--- prep-macos.bash
+++ prep-macos.bash
@@ -9,9 +9,9 @@ if [ ! -f $1 ]; then
   echo "file created with default settings: $1"
 fi
 }
-checkfiles "$DIR/config/default.conf" "$DIR/config-slash-default.conf.example"
-checkfiles "$DIR/config/settings.php" "$DIR/config-slash-settings-d7.php.example"
-checkfiles "$DIR/.env" "$DIR/dot-env.example"
+#checkfiles "$DIR/config/default.conf" "$DIR/config-slash-default.conf.example"
+#checkfiles "$DIR/config/settings.php" "$DIR/config-slash-settings-d7.php.example"
+#checkfiles "$DIR/.env" "$DIR/dot-env.example"
 
 HOSTMACHINE_IP="172.16.123.45"
 . $DIR/.env
@@ -20,7 +20,7 @@ sudo ifconfig lo0 alias $HOSTMACHINE_IP
 echo "setting up local dns resolution if needed"
 echo "nameserver $HOSTMACHINE_IP" | sudo tee /etc/resolver/test > /dev/null
 
-echo ""
-echo "if any files were created with the default settings, you will need to customize them before this stack is usable"
+#echo ""
+#echo "if any files were created with the default settings, you will need to customize them before this stack is usable"
 
 
