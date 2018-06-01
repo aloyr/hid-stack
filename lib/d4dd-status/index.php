fa17491e98bf0d24905454ecd4da4710e174fa23
Peter Carrero <peter.carrero@gmail.com>
2018-05-31 22:18:37 -0500

    d4dd: Enhance status screen

--- lib/d4dd-status/index.php
+++ lib/d4dd-status/index.php
@@ -1,3 +1,11 @@
 <a href="/opcache.php">Opcache status</a> <br>
 <a href="/redis.php">Redis status</a> <br>
-
+<br>
+available sites: <br>
+<?php foreach (glob('/var/www/*') as $folder): ?>
+  <?php if (explode('/', $folder)[3] !== 'html'): ?>
+    <?php $site = explode('/', $folder)[3] . '.test'; ?>
+    <?php print '<a href="http://' . $site . '">' . $site . '</a><br>'; ?>
+  <?php endif; ?>
+<?php endforeach; ?>
+ 
